46dcc7fb1a7dffcd69248fecab57c6ca2be69388
gabrielfs7 <gabrielfs7@gmail.com>
2019-05-07 22:38:46 +0200

    change port in order to make it work

--- Dockerfile
+++ Dockerfile
@@ -4,6 +4,6 @@ WORKDIR /usr/app
 
 COPY package*.json ./
 
-EXPOSE 9999
+EXPOSE 3000
 
 RUN npm install
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const http = require('http');
-const hostname = 'localhost';
-const port = '9999';
+const hostname = '127.0.0.1';
+const port = '3000';
 const server = http.createServer((req, res) => {
     let Block = require('./modules/blockchain/Block');
     let Blockchain = require('./modules/blockchain/Blockchain');
