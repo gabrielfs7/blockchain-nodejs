eaba88e38de0c4e8f7caf171c648d4d65fc574ef
gabrielfs7 <gabrielfs7@gmail.com>
2019-05-12 17:29:18 +0200

    signing transactions

--- modules/blockchain/Transaction.js
+++ modules/blockchain/Transaction.js
@@ -6,6 +6,32 @@ class Transaction
         this.toAddress = toAddress;
         this.amount = amount;
     }
+
+    sign(keyPair) {
+        if (keyPair.getPublic('hex') !== this.fromAddress) {
+            throw new Error('Invalid public key to sign');
+        }
+
+        const SHA256 = require('crypto-js/sha256');
+        const hash = SHA256(
+            this.fromAddress + 
+            this.toAddress + 
+            this.amount
+        ).toString()
+        const sign = keyPair.sign(hash, 'base64');
+
+        this.signature = sign.toDER('hex');
+    }
+
+    validateSignature() {
+        if (this.fromAddress === null) {
+            return;
+        }
+
+        if (!this.signature || this.signature.length === 0) {
+            throw new Error('Invalid transaction signature');
+        }
+    }
 }
 
 module.exports = Transaction;
