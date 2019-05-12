97720c29aecc43aee5566384f43e0ec3f23068d3
gabrielfs7 <gabrielfs7@gmail.com>
2019-05-12 18:18:30 +0200

    validate signature in transaction

--- modules/blockchain/KeyGen.js
+++ modules/blockchain/KeyGen.js
@@ -1,13 +1,18 @@
 class Keygen 
 {
-    constructor()
-    {
+    constructor() {
         const EC = require('elliptic').ec;
         this.ec = new EC('secp256k1');
     }
 
     generateKeys() {
         return this.ec.genKeyPair();
+    }
+
+    verify(fromAddress, hash, signature) {
+        const publicKey = this.ec.keyFromPublic(fromAddress, 'hex');
+
+        return publicKey.verify(hash, signature);
     }
 }
 
--- modules/blockchain/Transaction.js
+++ modules/blockchain/Transaction.js
@@ -1,10 +1,19 @@
 class Transaction 
 {
-    constructor(fromAddress, toAddress, amount)
-    {
+    constructor(fromAddress, toAddress, amount) {
         this.fromAddress = fromAddress;
         this.toAddress = toAddress;
         this.amount = amount;
+    }
+
+    generateHash() {
+        const SHA256 = require('crypto-js/sha256');
+
+        return SHA256(
+            this.fromAddress + 
+            this.toAddress + 
+            this.amount
+        ).toString()
     }
 
     sign(keyPair) {
@@ -12,24 +21,29 @@ class Transaction
             throw new Error('Invalid public key to sign');
         }
 
-        const SHA256 = require('crypto-js/sha256');
-        const hash = SHA256(
-            this.fromAddress + 
-            this.toAddress + 
-            this.amount
-        ).toString()
+        const hash = this.generateHash();
         const sign = keyPair.sign(hash, 'base64');
 
         this.signature = sign.toDER('hex');
     }
 
     validateSignature() {
+        /**
+         * If is MiningReward transaction, there is no signature.
+         */
         if (this.fromAddress === null) {
             return;
         }
 
         if (!this.signature || this.signature.length === 0) {
             throw new Error('Invalid transaction signature');
+        }
+
+        const Keygen = require('./KeyGen');
+        const keygen = new Keygen();
+
+        if (!keygen.verify(this.fromAddress, this.generateHash(), this.signature)) {
+            throw new Error('Invalid signature');
         }
     }
 }
